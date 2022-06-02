7f92aa93ad5baf43c682c1bc1dd062049c4c5181
Everton Pudim Jackson <everton@debian-BULLSEYE-live-builder-AMD64>
2022-06-02 07:38:34 -0300

    Updates

---
+++ _next/static/3OUsXLoSjzBiWs1Y8D6-G/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/95b64a6e-9562e008bb67a5b8.js","static/chunks/d7eeaac4-5486f34730382d4c.js","static/chunks/pages/index-0b35f255366ceb43.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
